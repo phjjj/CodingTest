0e0938e178ea8a93eddd0442fb369d696b1f066f
phjjj <102671646+phjjj@users.noreply.github.com>
2023-07-19 10:56:35 +0900

    [Bronze II] Title: 블랙잭, Time: 180 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.js
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -9,9 +9,12 @@ const m = Number(input[0].split(" ")[1]);
 const cards = input[1].split(" ").map(Number);
 
 let jack = 0;
-for (let i = 0; i < cards.length - 2; i++) {
-  for (let j = i + 1; j < cards.length - 1; j++) {
+for (let i = 0; i < cards.length; i++) {
+  for (let j = i + 1; j < cards.length; j++) {
     for (let k = j + 1; k < cards.length; k++) {
+      if (i == j || i == k || j == k) {
+        continue;
+      }
       let sum = cards[i] + cards[j] + cards[k];
       if (sum > jack && sum <= m) {
         jack = sum;
