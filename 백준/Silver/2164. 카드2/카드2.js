0fe204840570209f85000df5d5a4b219a397d6bb
phjjj <102671646+phjjj@users.noreply.github.com>
2023-07-27 10:57:25 +0900

    [Silver IV] Title: 카드2, Time: 524 ms, Memory: 89056 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "test.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const n = +input.join(" ");
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this._size = 0;
+  }
+
+  add(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+    }
+
+    this.tail = newNode;
+    this._size++;
+
+    return newNode;
+  }
+
+  getHead() {
+    return this.head.value;
+  }
+
+  removeHead() {
+    this.head = this.head.next;
+    this.head.prev = null;
+    this._size--;
+  }
+
+  getSize() {
+    return this._size;
+  }
+}
+
+const node = new LinkedList();
+
+for (let i = 1; i <= n; i++) {
+  node.add(i);
+}
+while (node.getSize() !== 1) {
+  node.removeHead();
+  node.add(node.getHead());
+  node.removeHead();
+}
+
+console.log(node.getHead());
