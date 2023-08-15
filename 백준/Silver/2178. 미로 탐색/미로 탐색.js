cfab46bd2fd1a94ad7673ee3fdfc3c0c917f6417
phjjj <102671646+phjjj@users.noreply.github.com>
2023-08-16 03:18:55 +0900

    [Silver I] Title: 미로 탐색, Time: 264 ms, Memory: 11904 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "test.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const [N, M] = input.shift().split(" ").map(Number);
+
+const map = input.map((v) => v.split("").map((x) => +x));
+const queue = [[0, 0, 1]];
+// 상, 하, 좌, 우
+const dir = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1],
+];
+
+while (queue.length) {
+  const [x, y, v] = queue.shift();
+
+  for (let i = 0; i < 4; i++) {
+    //i=0 일때 상 ,1일때 하, ...
+    const xPos = x + dir[i][0];
+    const yPos = y + dir[i][1];
+
+    if (0 <= xPos && yPos > -1 && xPos < M && yPos < N) {
+      if (map[yPos][xPos] === 1) {
+        map[yPos][xPos] = v + 1;
+        queue.push([xPos, yPos, v + 1]);
+      }
+    }
+  }
+}
+console.log(map[N - 1][M - 1]);
