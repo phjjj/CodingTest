e1ebfbfd8c03dd407c734ff564283a1c2d188b02
phjjj <102671646+phjjj@users.noreply.github.com>
2023-10-17 14:13:28 +0900

    [Silver IV] Title: 한윤정이 이탈리아에 가서 아이스크림을 사먹는데, Time: 212 ms, Memory: 14784 KB -BaekjoonHub

--- 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.js
+++ 백준/Silver/2422. 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/한윤정이 이탈리아에 가서 아이스크림을 사먹는데.js
@@ -1,25 +1,33 @@
+const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "test.txt";
-const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const [N, M] = input[0].split(" ").map((el) => +el);
-const arr = Array.from({ length: N + 1 }, () => Array(N + 1).fill(false));
-let count = 0;
+const [N, M] = input.shift().split(" ").map(Number);
+const arrM = input.map((v) => v.split(" ").map(Number));
 
-for (let i = 1; i <= M; i++) {
-  const [a, b] = input[i].split(" ").map((el) => +el);
+// 아이스크림은 1부터 시작하니까 6x6 2차원배열을 만든다
+let arr = Array.from({ length: N + 1 }, () => Array.from({ length: N + 1 }, () => false));
 
+for (let i = 0; i < arrM.length; i++) {
+  const [a, b] = arrM[i];
   arr[a][b] = true;
   arr[b][a] = true;
 }
 
+let cnt = 0;
 for (let i = 1; i <= N; i++) {
   for (let j = i + 1; j <= N; j++) {
     for (let k = j + 1; k <= N; k++) {
-      if (arr[i][k] || arr[j][k] || arr[i][j]) continue;
+      if (arr[i][j] || arr[i][k] || arr[j][k]) continue;
 
-      count += 1;
+      cnt++;
     }
   }
 }
+console.log(cnt);
+// 풀이
 
-console.log(count);
+// arr를 2차원 배열로 만든다
+// m개의 조합을 arr[a][b]=true , arr[b][a]=true  만들고
+// 3중 for문을 만들어 arr[a][b]와 같을 경우 continue
+// 조합에 해당되지 않으면 cnt++
