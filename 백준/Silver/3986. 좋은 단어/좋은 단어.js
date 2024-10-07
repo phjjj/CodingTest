c2867c6bb7216247b48d8f47b682aed6541f800f
phjjj <102671646+phjjj@users.noreply.github.com>
2024-10-07 18:37:41 +0900

    [Silver IV] Title: 좋은 단어, Time: 184 ms, Memory: 16200 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.js
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.js
@@ -1,30 +1,28 @@
+const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "test.txt";
-const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+const input = fs.readFileSync(filePath, "utf8").trim().split("\n");
 
 const N = +input.shift();
 const strArr = input;
 let answer = 0;
+
 for (let i = 0; i < N; i++) {
   let stack = [];
-  for (let j = 0; j < strArr[i].length; j++) {
-    if (strArr[i][j] === "A") {
-      if (stack[stack.length - 1] === "A") stack.pop();
-      else {
-        stack.push("A");
-      }
+  let str = strArr[i];
+
+  for (let j = 0; j < str.length; j++) {
+    const top = stack.length - 1;
+    if (stack[top] == str[j]) {
+      stack.pop();
+    } else {
+      stack.push(str[j]);
     }
-    if (strArr[i][j] === "B")
-      if (stack[stack.length - 1] === "B") stack.pop();
-      else {
-        stack.push("B");
-      }
   }
-  if (!stack.length) {
+  if (stack.length === 0) {
     answer++;
   }
 }
 console.log(answer);
-// 좋은단어를 찾기
-// 현재 검사하는 알파벳과 스택의 top을 비교해서 똑같을 경우 pop
-// 다를 경우 넣기
-// 마지막에 스택이 다 비었을 경우 좋은단어
+// 스택에 A,B를 쌓는다.
+// AA or BB 가 되는 경우, 스택은 pop 하고 단어는 shift
+// 스택이 비었을 경우 answer++
