eef57cb45a644b2e116b92c02d541b3ae261f53f
phjjj <102671646+phjjj@users.noreply.github.com>
2024-10-17 14:04:02 +0900

    [Silver II] Title: 유기농 배추, Time: 192 ms, Memory: 13396 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.js
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -1,62 +1,67 @@
-const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "test.txt";
-const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
 
-const T = +input.shift();
 
-function bfs(y, x, visited, map) {
-  let dir = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1],
-  ];
+  const T = +input.shift()
+  const dir = [[0,1],[0,-1],[1,0],[-1,0]]
 
-  let queue = [[x, y]];
+  function bfs(x,y,visited,map){
+    let queue =[[x,y]]
 
-  while (queue.length) {
-    const [x, y] = queue.shift();
+    while(queue.length){
+      let [curX,curY]=queue.shift()
 
-    if (!visited[y][x]) {
-      visited[y][x] = true;
-      for (let i = 0; i < 4; i++) {
-        const xPos = x + dir[i][0];
-        const yPos = y + dir[i][1];
-
-        if (xPos >= 0 && yPos >= 0 && xPos < map[0].length && yPos < map.length) {
-          if (map[yPos][xPos] === 1) {
-            queue.push([xPos, yPos]);
+      for(let i=0; i<4; i++){
+        const moveX = curX + dir[i][0]
+        const moveY = curY + dir[i][1]
+        
+        if( moveX >=0 && moveY >=0 && moveX < map.length && moveY < map[0].length){
+          if(!visited[moveX][moveY] && map[moveX][moveY]==1){
+          queue.push([moveX,moveY])
+          visited[moveX][moveY]=true
           }
         }
       }
     }
   }
-}
 
-function solution(map, visited) {
-  let cnt = 0;
-  for (let i = 0; i < map.length; i++) {
-    for (let j = 0; j < map[0].length; j++) {
-      if (map[i][j] === 1 && visited[i][j] === false) {
-        bfs(i, j, visited, map);
-        cnt++;
+  function  solution(map,visited){
+    let cnt = 0
+    for(let i =0; i<map.length; i++){
+      for(let j =0; j<map[0].length; j++){
+        
+        if(!visited[i][j] && map[i][j]==1){
+          bfs(i,j,visited,map)
+          visited[i][j]=true
+          cnt++
+        }
       }
     }
+    console.log(cnt);
   }
-  console.log(cnt);
-}
+  
+  for(let i =0; i<T; i++){
+    const [M,N,K] = input.shift().split(" ").map(Number)
+    const map = Array.from({length:N},()=>Array.from({length:M},()=>0))
+    const visited = Array.from({length:map.length},()=>Array.from({length:map[0].length},()=>false))
 
-for (let i = 0; i < T; i++) {
-  let [Y, X, K] = input.shift().split(" ");
-
-  // map 만들기
-  let visited = Array.from({ length: Y }, () => []).map((v) => (v = Array.from({ length: X }).fill(false)));
-  let map = Array.from({ length: Y }, () => []).map((v) => (v = Array.from({ length: X }).fill(0)));
-
-  for (let i = 0; i < K; i++) {
-    const [y, x] = input.shift().split(" ");
-    map[y][x] = 1;
+    for(let j=0; j<K; j++){
+      // 배추 위치 업데이트
+      const [x,y]=input.shift().split(" ").map(Number)
+      map[y][x]=1
+    }
+    
+    
+    solution(map,visited)
   }
 
-  solution(map, visited);
-}
+
+
+// 모든 map을 검사하여 1이 나오면 bfs를 통해 상하좌우로 검사를 때린다. (2중 for문 및 방문한 곳 제외)
+// 그럼, 1개의 지렁이로 어디까지 퍼지는지 알 수 있다. ()
+// 그래서 bfs를 돌린 횟수 만큼 카운트를 센다
+
