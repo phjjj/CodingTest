da6de753ac42f99bc9a523a183b11656574fc935
phjjj <102671646+phjjj@users.noreply.github.com>
2024-10-22 01:51:01 +0900

    [Silver III] Title: N과 M (7), Time: 900 ms, Memory: 175376 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.js
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.js
@@ -1,28 +1,36 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "test.txt";
-const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
 
-const [N, M] = input[0].split(" ").map(Number);
-const numArr = input[1]
+const [N, M] = input.shift().split(" ").map(Number);
+const numberArr = input
+  .shift()
   .split(" ")
   .map(Number)
   .sort((a, b) => a - b);
 const visited = Array.from({ length: N }, () => false);
-let seqArr = [];
-let str = "";
-function dfs(dep, n) {
-  // base condition
+let arr = [];
+let answer = "";
+
+dfs(0);
+
+function dfs(dep) {
+  // base condtion
   if (dep === M) {
-    str += `${seqArr.join(" ")}\n`;
-    return;
+    return (answer += arr.join(" ") + "\n");
   }
 
-  for (let i = 0; i < numArr.length; i++) {
-    seqArr.push(numArr[i]);
-    dfs(dep + 1, i);
-    seqArr.pop();
+  for (let i = 0; i < N; i++) {
+    // if (!visited[i]) {
+    // visited[i] = true;
+    arr.push(numberArr[i]);
+    dfs(dep + 1);
+    // visited[i] = false;
+    arr.pop();
+    // }
   }
 }
-dfs(0, 0);
-console.log(str);
-// 그 전 문제에서 중복을 허락 해줘서 visited 제거
-// 시간초과로 출력 방식 변경
+console.log(answer);
