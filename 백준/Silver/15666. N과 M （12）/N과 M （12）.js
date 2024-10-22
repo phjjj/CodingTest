766bddb1dd62733865b781f7e615091d990b1e24
phjjj <102671646+phjjj@users.noreply.github.com>
2024-10-22 20:16:17 +0900

    [Silver II] Title: N과 M (12), Time: 168 ms, Memory: 12200 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.js
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.js
@@ -1,26 +1,36 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "test.txt";
-const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
 
-const [N, M] = input[0].split(" ").map(Number);
-const numArr = input[1]
+const [N, M] = input.shift().split(" ").map(Number);
+const numArr = input
+  .shift()
   .split(" ")
   .map(Number)
   .sort((a, b) => a - b);
-const visited = Array.from({ length: N }, () => false);
-let seqArr = [];
+const visited = Array.from({ length: M }, () => false);
+
+const arr = [];
 let str = "";
+
+dfs(0, 0);
+console.log([...new Set(str.split("\n"))].join("\n"));
 function dfs(dep, n) {
   // base condition
   if (dep === M) {
-    str += `${seqArr.join(" ")}\n`;
-    return;
+    return (str += arr.join(" ") + "\n");
   }
 
   for (let i = n; i < numArr.length; i++) {
-    seqArr.push(numArr[i]);
+    // if (!visited[i]) {
+    //   visited[i] = true;
+    arr.push(numArr[i]);
     dfs(dep + 1, i);
-    seqArr.pop();
+    arr.pop();
+    //   // visited[i] = false;
+    // }
   }
 }
-dfs(0, 0);
-console.log([...new Set(str.split("\n"))].join("\n"));
