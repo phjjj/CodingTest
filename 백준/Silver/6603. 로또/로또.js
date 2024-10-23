fc3d6b0c47361bc393004704b74e4b815fd06985
phjjj <102671646+phjjj@users.noreply.github.com>
2024-10-23 11:51:15 +0900

    [Silver II] Title: 로또, Time: 152 ms, Memory: 10448 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.js
+++ 백준/Silver/6603. 로또/로또.js
@@ -1,36 +1,40 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "test.txt";
-const input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
 
-function solution(n, arr) {
-  const visited = Array.from({ length: n }, () => false);
-  let str = "";
-  let seqArr = [];
+for (let i = 0; i < input.length - 1; i++) {
+  const testCase = input[i].split(" ").map(Number);
+  const [K, ...numberArr] = testCase;
+  solution(K, numberArr);
+}
 
-  function dfs(dep, idx) {
+function solution(k, numberArr) {
+  let arr = [];
+  let answer = "";
+  const visited = Array.from({ length: k }, () => false);
+  dfs(0, 0);
+
+  function dfs(dep, n) {
     // base condition
-    if (dep == 6) {
-      str += `${seqArr.join(" ")}\n`;
+    if (dep === 6) {
+      answer += arr.join(" ") + "\n";
       return;
     }
-    for (let i = idx; i < n; i++) {
+
+    for (let i = n; i < k; i++) {
       if (!visited[i]) {
         visited[i] = true;
-        seqArr.push(arr[i]);
+        arr.push(numberArr[i]);
         dfs(dep + 1, i);
-        seqArr.pop();
+        arr.pop();
         visited[i] = false;
       }
     }
   }
-
-  dfs(0, 0);
-  console.log(str);
+  console.log(answer);
 }
-
-for (let i = 0; i < input.length; i++) {
-  if (input[i] == "0") break;
-
-  const [N, ...numArr] = input[i].split(" ").map(Number);
-
-  solution(N, numArr);
-}
+// shift로 0이 나오면 break
+// K개의 수에서 각 수를 순열로 만든다.
